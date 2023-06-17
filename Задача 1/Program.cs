03c92f7e2faede32cdfaa0282be7a259635f51a9
@Aidar Abitov <aydar099@gmail.com>
2023-06-17 13:10:36 +0300

    DZ3

---
+++ Задача 1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+Console.WriteLine ("введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int a = N%10 ;  
+int b = N%100/10 ;
+int c = N/10000  ;
+int d = N/1000%10;
+if (a == c) {
+    if (b==d) Console.WriteLine ("Это палиндром");
+} 
+else Console.WriteLine ("Не палиндром");
---
+++ Задача 3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear () ;
+Console.WriteLine("Введите целое число N") ;
+double N = double.Parse (Console.ReadLine ()!) ;
+double i = 1 ;
+while (i<=N) {
+    Console.Write (Math.Pow(i, 3));
+    Console.Write (", ");
+    i++;
+}
+
